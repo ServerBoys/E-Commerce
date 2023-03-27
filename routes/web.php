86afdf02d2ad182868c6fa8a72598803c5e04c1f
ServerBoys <pt70046@gmail.com>
2023-03-27 19:16:07 +0545

    initial commit

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,20 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    // return view('welcome');
+    echo "AS";
+});
+
+Route::get("/lol", function () {
     return view('welcome');
 });
+
+Route::get("/id", function ()  {
+    echo "ID";
+});
+
+Route::get("/pd", function ()  {
+    echo "ID";
+});
+
+?>
